2ce6c8e84b1f8209039f7949c72bad20be3a7bc6
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2022-04-10 20:04:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const games = require('./games.json');
-_iframe = `<iframe src="${gameURL}" scrolling="no" allow="autoplay; fullscreen; microphone; focus-without-user-activation *; screen-wake-lock; gamepad; clipboard-read; clipboard-write;" allowfullscreen="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" loading="eager" importance="high" class="game"></iframe>`;
+//const games = require('./games.json');
 function iframe(_in) {
   const page = document.getElementByid("game-div");
   page.innerHTML += _in;
