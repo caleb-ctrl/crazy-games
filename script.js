91145e35c27ce7873d382b5a2afbf814b3c6d9c0
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2022-04-11 07:42:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,19 +11,22 @@ const game = {
     "html": '<iframe src="https://games.crazygames.com/en_US/crazy-pixel-apocalypse-3/index.html" scrolling="no" allow="autoplay; fullscreen; microphone; focus-without-user-activation *; screen-wake-lock; gamepad; clipboard-read; clipboard-write;" allowfullscreen="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" loading="eager" importance="high" class="game"></iframe>'
   }
 }
+
+const _game1 = json(game)
+
 function iframe(_in) {
   const page = document.getElementByid("game-div");
   page.innerHTML += _in;
 }
 
 function crazyGames(url) {
-  if (url == game.cpa.name) {
-    iframe(game.cpa.html);
-  } else if (url == game.cpa3.name) {
-    iframe(game.cpa3.html);
+  if (url == _game1.cpa.name) {
+    iframe(_game1.cpa.html);
+  } else if (url == _game1.cpa3.name) {
+    iframe(_game1.cpa3.html);
   } else {
     print("error " + url);
   }
 }
 
-crazyGames(game.cpa.name);
+crazyGames(_game1.cpa.name);
