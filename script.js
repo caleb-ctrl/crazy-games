4abdf2f58c48981e69ee3acc0ecb15e33e34d2e9
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2022-04-10 19:43:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const games = require('./games.json');
-_iframe = `<iframe src="https://games.crazygames.com/en_US/crazy-pixel-apocalypse/index.html" scrolling="no" allow="autoplay; fullscreen; microphone; focus-without-user-activation *; screen-wake-lock; gamepad; clipboard-read; clipboard-write;" allowfullscreen="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" loading="eager" importance="high" class="game"></iframe>`;
-function iframe(url) {
+_iframe = `<iframe src="${gameURL}" scrolling="no" allow="autoplay; fullscreen; microphone; focus-without-user-activation *; screen-wake-lock; gamepad; clipboard-read; clipboard-write;" allowfullscreen="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" loading="eager" importance="high" class="game"></iframe>`;
+function iframe(_in) {
   const page = document.getElementByid("game-div");
+  page.innerHTML += _in;
 }
 
 function crazyGames(url) {
   if (url == games.cpa.name) {
-    iframe(games.cpa.url);
+    iframe(games.cpa.html);
   }
 }
 
