a78e129d6e36240cf6d5ed3228ff70678f9f5aef
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2022-05-10 07:16:11 -0500

    Update data.json.js

--- data.json.js
+++ data.json.js
@@ -11,3 +11,5 @@ const game = {
     "html": '<iframe src="https://games.crazygames.com/en_US/crazy-pixel-apocalypse-3/index.html" scrolling="no" allow="autoplay; fullscreen; microphone; focus-without-user-activation *; screen-wake-lock; gamepad; clipboard-read; clipboard-write;" allowfullscreen="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" loading="eager" importance="high" class="game"></iframe>'
   }
 }
+
+export game;
