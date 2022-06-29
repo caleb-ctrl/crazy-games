e246f6edda23ddfdd21aee3b645542a0d2bf9aa7
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2022-06-29 16:04:07 -0500

    added and fixed some things

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const _game = require('./games.json')
-const game = {
-  "cpa": {
-    "name": "Crazy Pixel Apocalypse",
-    "url": "https://www.crazygames.com/game/crazy-pixel-apocalypse",
-    "html": '<iframe src="https://games.crazygames.com/en_US/crazy-pixel-apocalypse/index.html" scrolling="no" allow="autoplay; fullscreen; microphone; focus-without-user-activation *; screen-wake-lock; gamepad; clipboard-read; clipboard-write;" allowfullscreen="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" loading="eager" importance="high" class="game"></iframe>'
-  },
-  "cpa3": {
-    "name": "Crazy Pixel Apocalypse 3",
-    "url": "https://www.crazygames.com/game/crazy-pixel-apocalypse-3",
-    "html": '<iframe src="https://games.crazygames.com/en_US/crazy-pixel-apocalypse-3/index.html" scrolling="no" allow="autoplay; fullscreen; microphone; focus-without-user-activation *; screen-wake-lock; gamepad; clipboard-read; clipboard-write;" allowfullscreen="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-scripts allow-same-origin allow-popups" loading="eager" importance="high" class="game"></iframe>'
-  }
-}
-
-const _game1 = JSON.parse(game)
+const game = require('./games.json')
 
 function iframe(_in) {
   const page = document.getElementByid("game-div");
@@ -20,10 +6,10 @@ function iframe(_in) {
 }
 
 function crazyGames(url) {
-  if (url == _game.cpa.name) {
-    iframe(_game.cpa.html);
-  } else if (url == _game.cpa3.name) {
-    iframe(_game.cpa3.html);
+  if (url == game.cpa.name) {
+    iframe(game.cpa.html);
+  } else if (url == game.cpa3.name) {
+    iframe(game.cpa3.html);
   } else {
     print("error " + url);
   }
@@ -33,13 +19,13 @@ function gaem() {
   gameCode = document.forms[0].gameCode.value;
   switch(gameCode) {
     case 'cpa':
-      crazyGames(_game.cpa.name);
+      crazyGames(game.cpa.name);
       break;
     case 'cpa2':
-      crazyGames(_game.cpa2.name);
+      crazyGames(game.cpa2.name);
       break;
     case 'cpa3':
-      crazyGames(_game.cpa3.name);
+      crazyGames(game.cpa3.name);
       break;
         case '':
       break;
@@ -57,8 +43,9 @@ function gaem() {
       break;
     case '':
       break;
-    
+    default:
+      crazyGames(game.cpa.name)
   }
 }
 
-//crazyGames(_game.cpa3.name);
+//crazyGames(game.cpa3.name);
